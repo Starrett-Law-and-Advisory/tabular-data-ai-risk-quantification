ad91c3ba8db2305451efbbd1ab47c31c154ff78a
IamUmairImran <umair.imran@axcelerate.ai>
2022-12-10 15:52:42 +0500

    Add function to generate histogram

--- generate_graphs.py
+++ generate_graphs.py
@@ -24,7 +24,7 @@ def plot_graph(config, data, x_label='n_steps', y_label='success_rate (param p i
 
     plt.text(1, 1, f'\u03BC={mean:.2f}, \u03C3={std:.2f}')
     # plot data line 
-    plt.plot(np.arange(1, config.n_steps_range+1), data)
+    plt.plot(np.arange(config.n_steps_range[0], config.n_steps_range[1]+1), data)
 
     # plot mean an std line
     plt.axhline(mean, color='r', linestyle='dashed')    
@@ -33,13 +33,62 @@ def plot_graph(config, data, x_label='n_steps', y_label='success_rate (param p i
     
     if display:
         plt.show()
+
+def plot_histogram(data, x_label='success_rate (param p in Bernoulli)', y_label='n_samples',
+               output_dir='visualization', fig_title='histogram', display=True):
+
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    plt.title(fig_title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    
+    bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    
+    plt.hist(data, bins=bins, edgecolor="black")    
+    plt.savefig(f'{output_dir}/{fig_title}.png')
+    
+    if display:
+        plt.show()
+    
+def generate_histogram(config):
+
+    low_n_steps, high_n_steps = config.n_steps_range
+    low_eta, high_eta = config.eta_range
+    low_lmbda, high_lmbda = config.lmbda_range
+    low_eta_decay, high_eta_decay = config.eta_decay_range
+
+    success_rates = []
+    for i in range(config.n_samples):
+
+        eta = np.random.uniform(low_eta, high_eta, 1)[0]
+        lmbda = np.random.uniform(low_lmbda, high_lmbda, 1)[0]
+        eta_decay = np.random.uniform(low_eta_decay, high_eta_decay, 1)[0]
+        n_steps = np.random.randint(low_n_steps, high_n_steps, 1, int)[0]
+
+        success_rate = test_3(
+            n_steps=int(n_steps),
+            eta=eta,
+            lambd=lmbda,
+            eta_decay=eta_decay,
+            n_train_samples=config.n_train_samples,
+            n_val_samples=config.n_val_samples
+        )
+
+        write_to_csv(config.csv_file, config, success_rate)
+        print("Success rate: {:.2f}%".format(100*success_rate))
+        success_rates.append(success_rate)
+
+    plot_histogram(success_rates, display=config.display)
     
 
-def generate_graph(config):
+
+def generate_linechart(config):
 
     success_rates = []
     
-    for ns in range(1, config.n_steps_range+1):
+    for ns in range(config.n_steps_range[0], config.n_steps_range[1]+1):
         success_rate = test_3(
             n_steps=ns,
             eta=config.eta,
@@ -57,6 +106,13 @@ def generate_graph(config):
     plot_graph(config, success_rates, display=config.display)
     
 
+def generate_graph(config):
+
+    # generate_linechart(config)    
+    generate_histogram(config)
+    
+    
+
 
 
 if __name__=="__main__":
@@ -64,10 +120,16 @@ if __name__=="__main__":
     parser.add_argument('-cf', '--csv-file', default='results.csv')
     parser.add_argument('-nts', '--n_train_samples', type=int)
     parser.add_argument('-nvs', '--n_val_samples', type=int)
-    parser.add_argument('-nsr', '--n_steps_range',  default=100, type=int)
+    parser.add_argument('-nsr', '--n_steps_range', nargs=2, default=[1, 10], type=int)
+    parser.add_argument('-er', '--eta_range', nargs=2, default=[0.2, 0.5], type=float)
     parser.add_argument('-e', '--eta', default=0.2, type=float)
     parser.add_argument('-l', '--lambd', default=0.2, type=float)
+    parser.add_argument('-lr', '--lmbda_range', nargs=2, default=[0.2, 0.5], type=float)
     parser.add_argument('-ed', '--eta_decay', default=0.9, type=float)
+    parser.add_argument('-edr', '--eta_decay_range',  nargs=2, default=[0.5, 0.9], type=float)
+    parser.add_argument('-ns', '--n_samples', default=100_000, type=int)
     parser.add_argument('-nd', '--no_display', action='store_false', dest='display', default=True)
+
     args = parser.parse_args()
+
     generate_graph(args)
